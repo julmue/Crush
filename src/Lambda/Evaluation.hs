99e3369246f7ee2c82609fb133f9109951632961
jules <jul.mue@hotmail.de>
2016-03-11 21:26:20 +0100

    added unname function to convert Lamda to NLL

--- src/Lambda/Evaluation.hs
+++ src/Lambda/Evaluation.hs
@@ -4,17 +4,18 @@ import Lambda.Named
 import Lambda.Nameless
 import Bound
 
+unname :: Eq a => Lambda a -> NLL a
+unname = foldL V (:$) lam
 
 
-
-nf :: Exp a -> Exp a
+nf :: NLL a -> NLL a
 nf e@V{} = e
 nf e@(L b) = L . toScope . nf . fromScope $ b
 nf e@(f :$ a) = case whnf f of
     L b -> nf (instantiate1 a b)
     f' -> nf f' :$ nf a
 
-whnf :: Exp a -> Exp a
+whnf :: NLL a -> NLL a
 whnf e@V{} = e
 whnf e@L{} = e
 whnf (f :$ a) = case whnf f of
--- src/Lambda/Nameless.hs
+++ src/Lambda/Nameless.hs
@@ -8,31 +8,31 @@ import Bound
 
 infixl 9 :$
 
-data Exp a
+data NLL a
     = V a
-    | Exp a :$ Exp a
-    | L (Scope () Exp a)
+    | NLL a :$ NLL a
+    | L (Scope () NLL a)
     deriving (Eq,Ord,Show,Read,Functor,Foldable,Traversable)
 
-instance Eq1 Exp
-instance Ord1 Exp
-instance Show1 Exp
-instance Read1 Exp
+instance Eq1 NLL
+instance Ord1 NLL
+instance Show1 NLL
+instance Read1 NLL
 
-instance Monad Exp where
+instance Monad NLL where
     return = V
     (V a) >>= f = f a
     (fun :$ arg) >>= f = (fun >>= f) :$ (arg >>= f)
     (L e) >>= f = L (e >>>= f)
 
-instance Applicative Exp where
+instance Applicative NLL where
     pure = V
     (<*>) = ap
 
 -- smart constructor
-lam :: Eq a => a -> Exp a -> Exp a
+lam :: Eq a => a -> NLL a -> NLL a
 lam v b = L (abstract1 v b)
 
 infix 0 !
-(!) :: Eq a => a -> Exp a -> Exp a
+(!) :: Eq a => a -> NLL a -> NLL a
 (!) = lam
