af1ba2076bd310e53ccc4f2d24a2aa4ed1e4ff28
jules <jul.mue@hotmail.de>
2016-04-24 17:51:37 +0200

    first working exe

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,4 +1,31 @@
 module Main where
 
+import Language.Lambda.Syntax.Named.Exp
+import qualified Language.Lambda.Syntax.Named.Parser as Parser
+import qualified Language.Lambda.Syntax.Named.Pretty as Pretty
+import Language.Lambda.Semantics.Named.BigStep
+
+import Control.Exception
+import Control.Monad.Except
+
+import qualified Bound.Unwrap as BU
+
+data Mode = Default | Trace | Normalize deriving (Show, Eq)
+
 main :: IO ()
-main = undefined
+main = do
+    let mode = Default
+    case mode of
+        Default -> do
+            stream <- getContents
+            case Parser.expression stream of
+                Left err -> putStrLn (show err)
+                Right expr -> putStrLn . Pretty.prettyPrint . normalOrder $ expr
+
+
+renderFresh :: BU.Fresh String -> String
+renderFresh f = BU.uname f ++ show (BU.fresh f)
+
+normalOrder :: Exp String -> Exp String
+normalOrder = mkNormalOrder renderFresh
+
